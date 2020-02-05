d6cf9bab28455457c89a34aa697448c66ba9b269
BruceAmende <bruceamende32@gmail.com>
2020-02-05 11:31:54 +0100

    Opdracht 1 Klaar!

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -2,8 +2,16 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
+window.onload = function(lampjes){
     //JOUW CODE HIER
+document.getElementById("light-top").style.backgroundColor = "red";
+
+document.getElementById("light-middle").style.backgroundColor = "orange";
+
+document.getElementById("light-bottom").style.backgroundColor = "green";
+
+
+
 
     
 }
